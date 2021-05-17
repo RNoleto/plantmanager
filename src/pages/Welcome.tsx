da4edb71fb7c9a20a3dc18e4e6719ab9a28dc748
Ramon Noleto <ramon.zap@hotmail.com>
2021-05-17 14:36:10 -0300

    importação de icone e google fonts josh

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,59 +1,78 @@
-import React, { useState } from 'react';
-import { SafeAreaView, Text, Image, TouchableOpacity, StyleSheet } from 'react-native';
+import React from 'react';
+import {
+    SafeAreaView,
+    Text,
+    Image,
+    TouchableOpacity,
+    StyleSheet,
+    Dimensions
+} from 'react-native';
+import { Feather } from '@expo/vector-icons';
 
 import wateringImg from '../assets/watering.png';
 import colors from '../styles/colors';
-import { Button } from '../components/Button';
+import fonts from '../styles/fonts';
 
 export function Welcome() {
-    const [visible, setVisible] = useState(false);
-
-    function handleVisibility() {
-        setVisible(true)
-    }
 
     return (
-        <SafeAreaView style={style.container}>
-            <Text style={style.title}>
+        <SafeAreaView style={styles.container}>
+            <Text style={styles.title}>
                 Gerencie {'\n'}
-                suas plantas {'\n'}
-                de forma fácil
+                suas plantas de{'\n'}
+                forma fácil
             </Text>
 
-            {
-                visible &&
-                <Image source={wateringImg} style={style.image} />
-            }
 
-            <Text style={style.subtitle}>
+            <Image
+                source={wateringImg}
+                style={styles.image}
+                resizeMode="contain"
+            />
+
+            <Text style={styles.subtitle}>
                 Não esqueça de regar suas plantas.
                 Nós cuidamos de lembrar você sempre que precisar.
             </Text>
 
-            <Button title=">" />
+            <TouchableOpacity
+                style={styles.button}
+                activeOpacity={0.7}
+            >
 
+                <Feather
+                    name='chevron-right'
+                    style={styles.buttonIcon}
+                />
+            </TouchableOpacity>
         </SafeAreaView>
     )
 }
 
-const style = StyleSheet.create({
+const styles = StyleSheet.create({
     container: {
         flex: 1,
         alignItems: 'center',
-        justifyContent: 'space-between'
+        justifyContent: 'space-around'
     },
     title: {
         fontSize: 32,
         fontWeight: 'bold',
         textAlign: 'center',
         color: colors.heading,
-        marginTop: 80
+        marginTop: 38,
+        fontFamily: fonts.heading,
+        lineHeight: 38
     },
     subtitle: {
         textAlign: 'center',
         fontSize: 18,
         paddingHorizontal: 20,
-        color: colors.heading
+        color: colors.heading,
+        fontFamily: fonts.text
+    },
+    image: {
+        height: Dimensions.get('window').width * 0.7
     },
     button: {
         backgroundColor: colors.green,
@@ -62,15 +81,10 @@ const style = StyleSheet.create({
         borderRadius: 16,
         marginBottom: 10,
         height: 56,
-        width: 56,
-        paddingHorizontal: 10
+        width: 56
     },
-    image: {
-        width: 292,
-        height: 284
-    },
-    buttonText: {
-        color: colors.white,
-        fontSize: 24
+    buttonIcon: {
+        fontSize: 32,
+        color: colors.white
     }
 });
