8accce30942b91fa921cea3c2ecfcad15acd05f8
Ramon Noleto <ramon.zap@hotmail.com>
2021-05-17 16:34:14 -0300

    Tela User Identification Criada

---
+++ src/pages/UserIdentification.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+    SafeAreaView,
+    StyleSheet
+} from 'react-native';
+
+export function UserIdentification() {
+    return (
+        <SafeAreaView style={styles.container}>
+
+        </SafeAreaView>
+
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+
+    }
+});
