bfb1a7d87f7df984ace50f909c751c9476cc1851
Ramon Noleto <ramon.zap@hotmail.com>
2021-05-18 17:13:48 -0300

    Importação de Icones e Fontes, Navegação entre paginas, animação de teclado aparecendo na tela, clicando fora do teclado faz o mesmo desaparecer, indicação visual para melhor experiencia do usuario, animação do emoji, criação de componente para o botãoe identificação de status do input

--- src/pages/UserIdentification.tsx
+++ src/pages/UserIdentification.tsx
@@ -1,5 +1,4 @@
-import AppLoading from 'expo-app-loading';
-import React from 'react';
+import React, { useState } from 'react';
 import {
     SafeAreaView,
     StyleSheet,
@@ -7,44 +6,85 @@ import {
     Text,
     TextInput,
     KeyboardAvoidingView,
-    Platform
+    TouchableWithoutFeedback,
+    Platform,
+    Touchable,
+    Keyboard
 } from 'react-native';
-
+import { useNavigation } from '@react-navigation/core';
 import { Button } from '../components/Button';
 
 import colors from '../styles/colors';
 import fonts from '../styles/fonts';
 
 export function UserIdentification() {
+    const [isFocused, setIsFocused] = useState(false);
+    const [isFilled, setIsFilled] = useState(false);
+    const [name, setName] = useState<string>();
+
+    const navigation = useNavigation();
+
+    function handleInputBlur() {
+        setIsFocused(false);
+        setIsFilled(!!name);
+
+    }
+
+    function handleInputFocus() {
+        setIsFocused(true)
+    }
+
+    function handleInputChange(value: string) {
+        setIsFilled(!!value);
+        setName(value);
+    }
+
+    function handleSubit() {
+        navigation.navigate('Confirmation');
+    }
+
     return (
         <SafeAreaView style={styles.container}>
             <KeyboardAvoidingView
                 style={styles.container}
                 behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
             >
-                <View style={styles.content}>
-                    <View style={styles.form}>
-                        <Text style={styles.emoji}>
-                            😀
-                        </Text>
-                        <Text style={styles.title}>
-                            Como podemos {'\n'}
-                            chamar você?
-                        </Text>
+                <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+                    <View style={styles.content}>
+                        <View style={styles.form}>
+                            <View style={styles.header}>
+                                <Text style={styles.emoji}>
+                                    {isFilled ? '😉' : '😀'}
+                                </Text>
+                                <Text style={styles.title}>
+                                    Como podemos {'\n'}
+                                chamar você?
+                            </Text>
+                            </View>
+                            <TextInput
+                                style={[
+                                    styles.input,
+                                    (isFocused || isFilled) &&
+                                    { borderColor: colors.green }
+                                ]}
+                                placeholder="Digite um nome"
+                                onBlur={handleInputBlur}
+                                onFocus={handleInputFocus}
+                                onChangeText={handleInputChange}
+                            />
+                            <View style={styles.footer}>
+                                <Button
+                                    title='Confirmar'
+                                    onPress={handleSubit}
+                                />
+                            </View>
+                        </View>
 
-                        <TextInput
-                            style={styles.input}
-                            placeholder="Digite um nome"
-                        />
-                        <Text style={styles.footer}>
-                            <Button />
-                        </Text>
                     </View>
-
-                </View>
+                </TouchableWithoutFeedback>
             </KeyboardAvoidingView>
 
-        </SafeAreaView>
+        </SafeAreaView >
 
     )
 }
@@ -54,18 +94,21 @@ const styles = StyleSheet.create({
         flex: 1,
         width: "100%",
         alignItems: 'center',
-        justifyContent: 'space-around'
+        justifyContent: 'space-around',
     },
     content: {
         flex: 1,
-        width: '100%'
+        width: '100%',
     },
     form: {
         flex: 1,
         justifyContent: 'center',
         paddingHorizontal: 54,
         alignItems: 'center',
-        width: '100%'
+        width: '100%',
+    },
+    header: {
+        alignItems: 'center',
     },
     emoji: {
         fontSize: 44,
@@ -77,7 +120,8 @@ const styles = StyleSheet.create({
         width: '100%',
         fontSize: 18,
         marginTop: 50,
-        padding: 'center'
+        padding: 10,
+        textAlign: 'center',
     },
     title: {
         fontSize: 24,
